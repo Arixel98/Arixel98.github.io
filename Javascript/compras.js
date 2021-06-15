93e4730c255a6c46e4d22ab74c81485849448623
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2021-06-15 18:50:06 -0500

    modificacion en compras js

--- Javascript/compras.js
+++ Javascript/compras.js
@@ -1,32 +1,44 @@
 //creamos un procedimiento para registrar
 alert("Bienvenido a Compras")
-function Registrar()
-{ //declarar variables para los controles
-    var N_titular=document.getElementById("N_titular");
-    var N_tarjeta=document.getElementById("N_tarjeta");
-    var fecha=document.getElementById("fecha");
-    var ccv=document.getElementById("ccv");
- 
-    
 
-   
-    if(N_titular.value==0){
+var getData = function()
+{ //declarar variables para los controles
+    var N_titular=document.getElementById("N_titular").value;
+    var N_tarjeta=document.getElementById("N_tarjeta").value;
+    var fecha=document.getElementById("fecha").value;
+    var ccv=document.getElementById("ccv").value;
+ 
+    if(N_titular==""){
         alert("Ingrese su nombre completo");
-        N_titular.focus();
-    }else if(N_tarjeta.value==""){
+        document.getElementById("N_titular").focus();
+    }else if(N_tarjeta==""){
         alert("Ingrese su numero de tarjeta")
-        N_tarjeta.focus();
-    }else if (fecha.selectedIndex==0){
+        document.getElementById("N_tarjeta").focus();
+    }else if (fecha==""){
         alert("Seleccione mes de vencimiento")
-        fecha.focus();
-    }else if(ccv.value==""){
+        document.getElementById("fecha").focus();
+    }else if(ccv ==""){
         alert("Ingrese su numero de cvv")
-        ccv.focus();
+        document.getElementById("ccv").focus();
     
 }else{
-    alert("No Finalizo el registro");
+    console.log(N_titular+" "+N_tarjeta+" "+fecha+" "+ccv);
+    document.getElementById("N_titular").value="";
+    document.getElementById("N_tarjeta").value="";
+    document.getElementById("fecha").value="";
+    document.getElementById("ccv").value="";
+    document.getElementById("N_titular").focus();
 }
 }
 
-Registrar()
+
+
+
+
+
+
+
+
+
+
 
