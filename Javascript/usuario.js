270191eba039288a43eec298122883066dbaf82d
sebas159fors <81602313+sebas159fors@users.noreply.github.com>
2021-06-15 22:24:25 -0500

    Update usuario.js

--- Javascript/usuario.js
+++ Javascript/usuario.js
@@ -1,10 +1,9 @@
 function go(){
 
 if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
-document.form.submit(); 
+        document.form.submit(); 
+    } 
+    else{ 
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    } 
 } 
-else{ 
- alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-} 
-} 
-onclick=go()
