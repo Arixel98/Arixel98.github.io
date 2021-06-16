e6a74016af65ae37981cfa4867abc326749586fd
sebas159fors <81602313+sebas159fors@users.noreply.github.com>
2021-06-15 22:36:17 -0500

    login para mas usuarios

--- Javascript/usuario.js
+++ Javascript/usuario.js
@@ -1,9 +1,14 @@
-function go(){
-
-if (document.getElementById=='contraseña' && document.getElementById=='usuario'){ 
-        document.form.submit(); 
+function Login(){ 
+    var done=0; 
+    var usuario=document.login.usuario.value; 
+    var password=document.login.password.value; 
+    if (usuario=="USUARIO1" && password=="CONTRASEÑA1") { 
+    window.location="TU_PAGINA_WEB.HTML"; 
     } 
-    else{ 
-         alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    if (usuario=="USUARIO2" && password=="CONTRASEÑA2") { 
+    window.location="TU_PAGINA_WEB.HTML"; 
     } 
-} 
+    if (usuario=="" && password=="") { 
+    window.location="errorpopup.html"; 
+    } 
+    } 
