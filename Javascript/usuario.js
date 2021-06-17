0e2d7290e87ada21522cfb92295f1b845675b914
sebas159fors <81602313+sebas159fors@users.noreply.github.com>
2021-06-17 01:10:11 -0500

    c

--- Javascript/usuario.js
+++ Javascript/usuario.js
@@ -1,25 +1,28 @@
 alert("Ingresa con correo usuario - Mortal")
-var getData = function()
+var getData = function ()
 {
-    var usuario = document.getElementById("usuario").value;
-    var contraseña = document.getElementById("contraseña").value;
-    var ingresar = document.getElementById("ingresar").value
-    
-    
-    if(usuario==""){
-        alert("Ingrese su usuario completo");
-        document.getElementById("usuario").focus();
-    }
-    else if(contraseña==""){
-        alert("Ingrese su contraseña")
-        document.getElementById("contraseña").focus();
-    }
-    else if(ingresar==""){
-    alert("continuemos con tu compra - Mortal").focus();
-    }
-    else{
-    console.log(usuario+" "+contraseña+"")
-    }}
+var usuario = document.getElementById("usuario").value;
+var contraseña = document.getElementById("contraseña").value;
+var ingresar = document.getElementById("ingresar").value
+
+
+if(usuario==""){
+    alert("Ingrese su usuario completo");
+    document.getElementById("usuario").focus();
+}
+else if(contraseña==""){
+    alert("Ingrese su contraseña")
+    document.getElementById("contraseña").focus();
+}
+else if(ingresar==""){
+alert("continuemos con tu compra - Mortal").focus();
+}
+else{
+console.log(usuario+" "+contraseña+"")
+}}
+
+
+//¡NO BORRAR JAMAS JAMAS DE LOS JAMASES!
 
 
 
