4de42de980813af3601c0432c508072c3e68282c
jeanpier12 <81602541+jeanpier12@users.noreply.github.com>
2021-06-17 15:57:28 -0500

    index

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,28 +1,49 @@
 
+window.addEventListener('load',function(){
+console.log('cargo el contenido');
+
 
 var ingresar =prompt("Ingrese su nombre");
 var cambio =document.querySelector("h1").innerHTML= " Bienbenido " +ingresar+ " a la paguina de JJCSS";
+
+})
+
 function bienvenido(){
 document.write(cambio );
 }
 
 
 
-  
-function img(){
-    var imagen =document.getElementById("prin");
-    imagen.src="/imagenes/nueva.png"
-}
 
-
- function ramon(){
-   var imagen =document.getElementsById("img1");
-    imagen.src="/imagenes/PUBLICIDAD PRINCIPAL.png"
-
-
-} 
-
-function remon(){
-     var imagen =document.getElementsById("img2");
-    imagen.src="/imagenes/robot.png"
-}
+window.addEventListener('load',function(){
+    console.log('cargo el contenido');
+    
+    
+    var imagenes=[];
+    
+    imagenes[0]="../imagenes/PUBLICIDAD PRINCIPAL.png";
+    imagenes[1]="../imagenes/nueva.png";
+    imagenes[2]="../imagenes/imagen3.jpg";
+    
+     var indice=0;
+    
+     
+    
+    function cambiarImg(){
+    
+        document.juas.src =imagenes[indice]; 
+    
+       if (indice<2) {
+           indice++
+       }
+       else{
+           indice=0;
+       }
+          
+             
+    }
+    setInterval(cambiarImg,2000);
+    
+    });
+    
+    
