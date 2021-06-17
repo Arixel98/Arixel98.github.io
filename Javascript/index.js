f12ee7d2903e5e8a3819a1bac7f29fce8df48f9a
jeanpier12 <81602541+jeanpier12@users.noreply.github.com>
2021-06-16 19:54:52 -0500

    cambios

---
+++ Javascript/index.js
@@ -0,0 +1,15 @@
+
+
+var ingresar =prompt("Ingrese su nombre");
+var cambio =document.querySelector("h1").innerHTML= " Bienbenido " +ingresar+ " a la paguina de JJCSS";
+function bienvenido(){
+document.write(cambio );
+}
+
+
+
+  
+function img(){
+    var imagen =document.getElementById("prin");
+    imagen.src="/imagenes/nueva.png"
+}
