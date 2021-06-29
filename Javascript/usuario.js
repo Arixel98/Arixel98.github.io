5d1cf0def8b1458cc29a14358c11cfed003b1393
jeanpier12 <81602541+jeanpier12@users.noreply.github.com>
2021-06-28 19:08:39 -0500

    he creado el usuario xd

--- Javascript/usuario.js
+++ Javascript/usuario.js
@@ -1,25 +1,50 @@
-alert("Ingresa con correo usuario - Mortal")
-var getData = function ()
-{
-var usuario = document.getElementById("usuario").value;
-var contraseña = document.getElementById("contraseña").value;
-var ingresar = document.getElementById("ingresar").value
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDtbHgQqPNuU3wwawu11PkmV20DCZfOw1I",
+    authDomain: "jjcss-ff566.firebaseapp.com",
+    projectId: "jjcss-ff566",
+    storageBucket: "jjcss-ff566.appspot.com",
+    messagingSenderId: "917742652533",
+    appId: "1:917742652533:web:6988f31b01a0e94a617c3c",
+    measurementId: "G-VC4CEQFVBS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
 
 
-if(usuario==""){
-    alert("Ingrese su usuario completo");
-    document.getElementById("usuario").focus();
-}
-else if(contraseña==""){
-    alert("Ingrese su contraseña")
-    document.getElementById("contraseña").focus();
-}
-else if(ingresar==""){
-alert("continuemos con tu compra - Mortal").focus();
-}
-else{
-console.log(usuario+" "+contraseña+"")
-}}
+
+
+
+
+
+
+
+
+
+
+// alert("Ingresa con correo usuario - Mortal")
+// var getData = function ()
+// {
+// var usuario = document.getElementById("usuario").value;
+// var contraseña = document.getElementById("contraseña").value;
+// var ingresar = document.getElementById("ingresar").value
+
+
+// if(usuario==""){
+//     alert("Ingrese su usuario completo");
+//     document.getElementById("usuario").focus();
+// }
+// else if(contraseña==""){
+//     alert("Ingrese su contraseña")
+//     document.getElementById("contraseña").focus();
+// }
+// else if(ingresar==""){
+// alert("continuemos con tu compra - Mortal").focus();
+// }
+// else{
+// console.log(usuario+" "+contraseña+"")
+// }}
 
 
 //¡NO BORRAR JAMAS JAMAS DE LOS JAMASES!
