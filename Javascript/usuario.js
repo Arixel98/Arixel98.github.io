80efea01a2ca563bf8c25d0c29cdd8af0a0ddb36
jeanpier12 <81602541+jeanpier12@users.noreply.github.com>
2021-07-02 18:28:57 -0500

    usuario terminado

--- Javascript/usuario.js
+++ Javascript/usuario.js
@@ -1,58 +1,81 @@
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDtbHgQqPNuU3wwawu11PkmV20DCZfOw1I",
-    authDomain: "jjcss-ff566.firebaseapp.com",
-    projectId: "jjcss-ff566",
-    storageBucket: "jjcss-ff566.appspot.com",
-    messagingSenderId: "917742652533",
-    appId: "1:917742652533:web:6988f31b01a0e94a617c3c",
-    measurementId: "G-VC4CEQFVBS"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  // firebase.analytics();
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDtbHgQqPNuU3wwawu11PkmV20DCZfOw1I",
+//     authDomain: "jjcss-ff566.firebaseapp.com",
+//     projectId: "jjcss-ff566",
+//     storageBucket: "jjcss-ff566.appspot.com",
+//     messagingSenderId: "917742652533",
+//     appId: "1:917742652533:web:6988f31b01a0e94a617c3c",
+//     measurementId: "G-VC4CEQFVBS"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//    firebase.analytics();
+  
+// // codigo para crear o registrar el usuario
+// function createUser(){
+//   var email=document.getElementById("txtcorreo").value;
+//   var contra=document.getElementById("txtcontra").value;
+//   firebase.auth().createUserWithEmailAndPassword(email, contra)
+// .then((userCredential) => {
+//   // Signed in
+//   var user = userCredential.user;
+//   alert("Se resguistro correctamente");
+//   // ...
+// })
+// .catch((error) => {
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   alert("ocurrio un error");
+//   // ..
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-// alert("Ingresa con correo usuario - Mortal")
-// var getData = function ()
-// {
-// var usuario = document.getElementById("usuario").value;
-// var contraseña = document.getElementById("contraseña").value;
-// var ingresar = document.getElementById("ingresar").value
-
-
-// if(usuario==""){
-//     alert("Ingrese su usuario completo");
-//     document.getElementById("usuario").focus();
 // }
-// else if(contraseña==""){
-//     alert("Ingrese su contraseña")
-//     document.getElementById("contraseña").focus();
-// }
-// else if(ingresar==""){
-// alert("continuemos con tu compra - Mortal").focus();
-// }
-// else{
-// console.log(usuario+" "+contraseña+"")
-// }}
+
+// // acceso de usuario existente
+// function acceso(){
+// let email=document.getElementById("12").value;
+// let pass=document.getElementById("13").value;
+// firebase.auth().signInWithEmailAndPassword(email, pass)
+// .then((userCredential) => {
+// // Signed in
+// var user = userCredential.user;
+// alert("Accediste");
+// })
+// .catch((error) => {
+// var errorCode = error.code;
+// var errorMessage = error.message;
+// alert(errorMessage);
+// });
+
+//  };
+// // observador de estado de autenticacion
+
+//   firebase.auth().onAuthStateChanged((user) => {
+//       if (user) {
+//         // User is signed in, see docs for a list of available properties
+//         // https://firebase.google.com/docs/reference/js/firebase.User
+//         var uid= user.uid;
+//         document.getElementById("login").innerHTML="Logueado " +user.email;
+//         // ...
+//       } else {
+//         // User is signed out
+//         // ...
+//         document.getElementById("login").innerHTML="no Logueado " 
+//       }
+//     });
 
 
-//¡NO BORRAR JAMAS JAMAS DE LOS JAMASES!
 
 
 
 
-//¡NO BORRAR JAMAS JAMAS DE LOS JAMASES!
+
+
+
+
+
+
 
 
 
