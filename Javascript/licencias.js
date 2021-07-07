9a25c7f7b6d54aa32a45b9da17395faf7d9354aa
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2021-07-06 20:52:53 -0500

    Correción licencias js

--- Javascript/licencias.js
+++ Javascript/licencias.js
@@ -1,35 +1,35 @@
 
 function cuenta() {
-var licenciaM=50;
-var licenciasA=100
-var licenciaB=80
-var licenciaK=90
-let dinero=prompt("cuanto dinero tines?");
-   dinero=parseInt(dinero)
-
-if (dinero>=50& dinero<80){
-    alert(" puedes comprarte una licencia de Microsoft ");
-    alert("te sobraria" + " S/" + (dinero - 50))
-} 
-
-else if (dinero>= 80 & dinero<90){
-    alert(" puedes comprarte una licencia bitdefender");
-    alert(" te sobra " + " S/" +(dinero - 80));
-}
-
-                                                                                                                                                              
-    else if (dinero>= 90 & dinero<100){
-        alert(" puedes comprarte una licencia Kaspersky");
-        alert(" te sobra " + " S/" +(dinero - 90));
+    var licenciaM=50;
+    var licenciasA=100
+    var licenciaB=80
+    var licenciaK=90
+    let dinero=prompt("cuanto dinero tines?");
+       dinero=parseInt(dinero)
+    
+    if (dinero>=50& dinero<80){
+        alert(" puedes comprarte una licencia de Microsoft ");
+        alert("te sobraria" + " S/" + (dinero - 50))
+    } 
+    
+    else if (dinero>= 80 & dinero<90){
+        alert(" puedes comprarte una licencia bitdefender");
+        alert(" te sobra " + " S/" +(dinero - 80));
     }
-    else if (dinero >=100 & dinero < 10000000000){
-alert("puedes comprarte una licencia de antivirus ");
-alert(" te sobra " +  " S/" +(dinero - 100));
-    }
-    else {
-        if(dinero==dinero){
-            alert(" lo siento  no te alcanza para ninguna licencia ");
+    
+                                                                                                                                                                  
+        else if (dinero>= 90 & dinero<100){
+            alert(" puedes comprarte una licencia Kaspersky");
+            alert(" te sobra " + " S/" +(dinero - 90));
+        }
+        else if (dinero >=100 & dinero < 10000000000){
+    alert("puedes comprarte una licencia de antivirus ");
+    alert(" te sobra " +  " S/" +(dinero - 100));
+        }
+        else {
+            if(dinero==dinero){
+                alert(" lo siento  no te alcanza para ninguna licencia ");
+            }
         }
     }
-}
-window.addEventListener("load",cuenta)
+    window.addEventListener("load",cuenta)
