3c5676f4ca718d9669270b4325d7606d0fd15f11
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2021-07-06 20:52:32 -0500

    Correción entrega js

--- Javascript/Entrega.js
+++ Javascript/Entrega.js
@@ -1,29 +1,27 @@
-alert("Ingresa los datos de entrega")
 
-var metaData = function() { //declarar variables para los controles
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var d_entrega = document.getElementById("d_entrega").value;
-    var siguiente = document.getElementById("siguiente").value;
 
-    if (nombre == "") {
+
+var getData = function()
+{ //declarar variables para los controles
+    var nombre=document.getElementById("nombre").value;
+    var apellido=document.getElementById("apellido").value;
+    var d_entrega=document.getElementById("d-entrega").value;
+ 
+    if(nombre==""){
         alert("Ingrese su nombre completo");
         document.getElementById("nombre").focus();
-    } else if (apellido == "") {
+    }else if(apellido==""){
         alert("Ingrese su apellido completo")
         document.getElementById("apellido").focus();
-    } else if (d_entrega == "") {
+    }else if (d_entrega==""){
         alert("Ingrese su direccion de entrega")
         document.getElementById("d_entrega").focus();
-    } else if (siguiente == "") {
-        alert("Ingrese su direccion de entrega")
-        document.getElementById("siguiente").focus();
 
-    } else {
-        console.log(nombre + " " + apellido + " " + d_entrega);
-        document.getElementById("nombre").value = "";
-        document.getElementById("apellido").value = "";
-        document.getElementById("d_entrega").value = "";
-        document.getElementById("siguiente").focus();
-    }
+    }else{
+        console.log(nombre+" "+apellido+" "+d_entrega);
+        document.getElementById("nombre").value="";
+        document.getElementById("apellido").value="";
+        document.getElementById("d_entrega").value="";
+        document.getElementById("nombre").focus();
+}
 }
