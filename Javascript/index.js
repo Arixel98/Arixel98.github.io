49bf794cddbd4f30fc770cbf998e10e352460711
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2021-07-06 20:52:46 -0500

    Correción index js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,35 @@
-var ingresar = prompt("Ingrese su nombre");
-var cambio = document.querySelector("h1").innerHTML = " Bienbenido " + ingresar + " a la paguina de JJCSS";
 
-function bienvenido() {
-    document.write(cambio);
-}
-
-
-
-
-function img() {
-    var imagen = document.getElementById("prin");
-    imagen.src = "/imagenes/nueva.png"
-}
+window.addEventListener('load',function(){
+    console.log('cargo el contenido');
+    
+    
+    
+    
+        var imagenes=[];
+        
+        imagenes[0]="imagenes/PUBLICIDAD PRINCIPAL.png";
+        imagenes[1]="../imagenes/nueva.png";
+        imagenes[2]="../imagenes/imagen3.jpg";
+        
+         var indice=0;
+        
+         
+        
+        function cambiarImg(){
+        
+            document.juas.src =imagenes[indice]; 
+        
+           if (indice<2) {
+               indice++
+           }
+           else{
+               indice=0;
+           }
+              
+                 
+        }
+        setInterval(cambiarImg,2000);
+    
+        var ingresar =prompt("Ingrese su nombre");
+        var cambio =document.querySelector("h1").innerHTML= " Bienbenido " +ingresar+ " a la paguina de JJCSS";
+    })
