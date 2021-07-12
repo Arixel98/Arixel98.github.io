16d9e6c4331224e36174e23682d9bb51328f4231
sebas159fors <81602313+sebas159fors@users.noreply.github.com>
2021-07-12 16:29:42 -0500

    Matenme 2

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,14 +4,16 @@ const baseDeDatos = firebase.firestore();
 const Index = document.getElementById("Publicidad");
 const Summer = document.getElementById("Summer");
 const Articulos = document.getElementById("Articulos");
-
+const Redes = document.getElementById("Redes")
+const Imagen8 = document.getElementById("Imagen8")
+const Robot = document.getElementById("Robot")
 
 const obteniendoImagenes = (funcionObtener) => {
     baseDeDatos.collection("imagenes").onSnapshot(funcionObtener);
 }
 
-window.addEventListener('DOMContentLoaded', mostrarPublicidad());
-window.addEventListener('DOMContentLoaded', mostrarSummer,mostrarArticulos());
+window.addEventListener('DOMContentLoaded', mostrarPublicidad(),mostrarSummer(),mostrarArticulos(),mostrarRedes(),mostrarImagen8(),mostrarRobot());
+
 
 function mostrarPublicidad(){
     obteniendoImagenes((listaDeImagenes)=>{
@@ -25,39 +27,39 @@ function mostrarPublicidad(){
 )
 }
 
-window.addEventListener('load',function(){
+window.addEventListener('DOMContentLoaded',function Banner(){
     console.log('cargo el contenido');
-    
-    
-    
-    
+
+
+
+
         var imagenes=[];
-        
+    
         imagenes[0]="imagenes/PUBLICIDAD PRINCIPAL.png";
         imagenes[1]="../imagenes/nueva.png";
         imagenes[2]="../imagenes/imagen3.jpg";
-        
+    
          var indice=0;
+    
         
-         
-        
+    
         function cambiarImg(){
-        
+    
             document.juas.src =imagenes[indice]; 
-        
+    
            if (indice<2) {
                indice++
            }
            else{
                indice=0;
            }
-              
-                 
+            
+                
         }
         setInterval(cambiarImg,2000);
-    
+
         var ingresar =prompt("Ingrese su nombre");
-        var cambio =document.querySelector("h1").innerHTML= " Bienbenido " +ingresar+ " a la paguina de JJCSS";
+        var cambio =document.querySelector("h1").innerHTML= " Bienvenido " +ingresar+ " a la pagína de JJCSS";
     })
 
     function mostrarSummer(){
@@ -85,4 +87,43 @@ window.addEventListener('load',function(){
                    )
             }
         )
+    }
+
+
+    function mostrarRedes(){
+        obteniendoImagenes((listaDeImagenes)=>{
+            Redes.innerHTML=``;
+               listaDeImagenes.forEach(
+                   (o) => {
+                       o = Redes.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/redes1-sin%20fondo.png?alt=media&token=e3d4264c-7186-4862-9cb7-2daa475c8255 width="100px"/>`;
+                   }
+               )
+        }
+    )
+    }
+
+
+    function mostrarImagen8(){
+        obteniendoImagenes((listaDeImagenes)=>{
+            Imagen8.innerHTML=``;
+               listaDeImagenes.forEach(
+                   (o) => {
+                       o = Imagen8.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/image__8%20-%20sin%20fondo.png?alt=media&token=8286c064-6dd0-4625-a339-b0ef9262467a width="280px" height="80px"/>`;
+                   }
+               )
+        }
+    )
+    }
+
+
+    function mostrarRobot(){
+        obteniendoImagenes((listaDeImagenes)=>{
+            Robot.innerHTML=``;
+               listaDeImagenes.forEach(
+                   (o) => {
+                       o = Robot.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/robot.png?alt=media&token=3a6e599b-8993-4646-bfe3-b6fba3587539 width="200px" height="200px"/>`;
+                   }
+               )
+        }
+    )
     }
