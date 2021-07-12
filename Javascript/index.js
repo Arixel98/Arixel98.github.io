e518cf1c67325b1f8fa78b007e7bbac8ee65bbfe
sebas159fors <81602313+sebas159fors@users.noreply.github.com>
2021-07-11 23:55:11 -0500

    Version Matame o Me mueiro

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,19 +2,23 @@ const storageRef = firebase.storage().ref();
 
 const baseDeDatos = firebase.firestore();
 const Index = document.getElementById("Publicidad");
+const Summer = document.getElementById("Summer");
+const Articulos = document.getElementById("Articulos");
+
 
 const obteniendoImagenes = (funcionObtener) => {
     baseDeDatos.collection("imagenes").onSnapshot(funcionObtener);
 }
 
 window.addEventListener('DOMContentLoaded', mostrarPublicidad());
+window.addEventListener('DOMContentLoaded', mostrarSummer,mostrarArticulos());
 
 function mostrarPublicidad(){
     obteniendoImagenes((listaDeImagenes)=>{
         Index.innerHTML=``;
            listaDeImagenes.forEach(
-               (e) => {
-                   e = Index.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/publicidad.png?alt=media&token=04edc1cc-ee45-4501-a868-40c9922b98c3>`;
+               (a) => {
+                   a = Index.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/publicidad.png?alt=media&token=04edc1cc-ee45-4501-a868-40c9922b98c3>`;
                }
            )
     }
@@ -55,3 +59,30 @@ window.addEventListener('load',function(){
         var ingresar =prompt("Ingrese su nombre");
         var cambio =document.querySelector("h1").innerHTML= " Bienbenido " +ingresar+ " a la paguina de JJCSS";
     })
+
+    function mostrarSummer(){
+        obteniendoImagenes(
+            (listaDeImagenes)=>{
+                Summer.innerHTML=``;
+                   listaDeImagenes.forEach(
+                       (e) => {
+                           e = Summer.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/sumer.png?alt=media&token=f37c2085-b58d-464f-a05e-2ea6cdbebbe2 width="390px" height="390px"/>`;
+                       }
+                   )
+            }
+        )
+    }
+
+
+    function mostrarArticulos(){
+        obteniendoImagenes(
+            (listaDeImagenes)=>{
+                Articulos.innerHTML=``;
+                   listaDeImagenes.forEach(
+                       (i) => {
+                           i = Articulos.innerHTML = `<img src=https://firebasestorage.googleapis.com/v0/b/jjcss-tech-dff98.appspot.com/o/articulosegundario.png?alt=media&token=e6c14cbc-852f-4374-89d9-24fee7d9f71d width="390px" height="390px"/>`;
+                       }
+                   )
+            }
+        )
+    }
