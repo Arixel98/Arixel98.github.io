5dcb23a024f3a94e135ec7b57b88889faafc546f
C-Valerio <81839625+C-Valerio@users.noreply.github.com>
2021-07-13 11:25:13 -0500

    js

--- Javascript/nosotros.js
+++ Javascript/nosotros.js
@@ -12,3 +12,47 @@ btnCerrarPopup.addEventListener('click', function() {
     overlay.classList.remove('active');
     popup.classList.remove('active');
 });
+
+const db = firebase.firestore();
+
+const taskForm = document.getElementById("task-Form");
+
+const saveTask = (Nombre, Teléfono) =>
+    db.collection("Te Contacto").doc().set({
+        Name: Nombre,
+        Phone: Teléfono,
+    })
+
+taskForm.addEventListener("submit", async(e) => {
+    e.preventDefault()
+    const cName = taskForm["over-Nombre"].value;
+    const cPhone = taskForm["over-Teléfono"].value;
+
+    await saveTask(cName, cPhone);
+
+    taskForm.reset();
+
+})
+
+
+
+
+
+
+// //conexion a base de datos
+// const db = firebase.firestore();
+
+// //obtengo el formulario web y el contenedor
+// const taskForm = document.getElementById("task-form");
+// // const taskContainer = document.getElementById("task-container");
+
+// //funcion a la vieja
+// function saveTask(Nombre, Teléfono) {
+//     db.collection("Te Contacto").doc().set({
+//         Name: Nombre,
+//         Phone: Teléfono
+//     })
+// }
+
+// let estadoEditar = false;
+// let idAEditar = "";;
